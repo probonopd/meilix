c0975b5fee9b5af19a9cd9c85bb13d875d88be2d
probonopd <probonopd@users.noreply.github.com>
2019-07-26 16:44:25 +0200

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo "# Remastered" >> /etc/os-release
+# Place commands that should run in the chrooted system here
+
+# echo "# Remastered" >> /etc/os-release # Don't do this, it disturbs add-apt-repository
